243d433b2d5dd2ebf8312f9888a2db684702910d
Zanelato <127983241+pdrzanFilho@users.noreply.github.com>
2023-04-04 09:15:39 -0400

    Mudanças em alguns estados, adicionados operadores

--- scanner.cpp
+++ scanner.cpp
@@ -56,8 +56,18 @@ Scanner::nextToken()
         
         switch (state)
         {
+            /*
+                Observação:
+                Nós dividimos os tipos de estados de acordo com seu número.
+
+                Identificadores - state começa com o número 1
+                Números Inteiros - state começa com o número 2
+                Operadoes - state começa com o número 3
+                Separadores - state começa com o número 4
+                Palavras Reservadas - state começa com o número 5
+                Comentárois - state começa com o número 6
+            */
             case 0:
-                
                 //se for fim de arquivo
                 if (input[pos] == '\0'){
                     tok = new Token(END_OF_FILE);
@@ -71,7 +81,7 @@ Scanner::nextToken()
                 else if(isdigit(input[pos]))
                     state = 11;
 
-                //se for comentario
+                //se for comentario ou o operador '/'
                 else if(input[pos]=='/')
                     state = 101;
                 
@@ -87,6 +97,31 @@ Scanner::nextToken()
                          input[pos]==',')
                     state = 20;
 
+                // se for operadores
+                else if(input[pos] == '&')
+                    state = 31;
+
+                else if(input[pos] == '>')
+                    state = 33;
+
+                else if(input[pos] == '<')
+                    state = 34;
+
+                else if(input[pos] == '+')
+                    state = 35;
+
+                else if(input[pos] == '-')
+                    state = 36;
+
+                else if(input[pos] == '*')
+                    state = 37;
+
+                else if(input[pos] == '=')
+                    state = 38;
+
+                else if(input[pos] == '!')
+                    state = 301;
+                // operador 
                 //TODO
 
 
@@ -122,15 +157,101 @@ Scanner::nextToken()
             case 20:
                 //caso seja um reparador
                 tok = new Token(SEP);
+                return tok;
+
+            case 30:
+                // retorna operador '/'
+                tok = new Token(DIV);
+                pos--;
+                return tok;
+
+            case 31:
+                // analisa se é o operador &
+                if(input[pos] == '&')
+                    state = 32;
+                else
+                    lexicalError("Lexema não pertencente à linguagem MiniJava");
                 pos++;
+                break;
+            
+            case 32:
+                // retorna token &&(AND)
+                tok = new Token(AND);
+                return tok;
+
+            case 33:
+                // retorna token > (GT)
+                tok = new Token(GT);
+                return tok;
+
+            case 34:
+                // retorna token < (LT)
+                tok = new Token(LT);
+                return tok;
+
+            case 35:
+                // retorna token + (PLUS)
+                tok = new Token(PLUS);
+                return tok;
+            
+            case 36:
+                // retorna token - (MINUS)
+                tok = new Token(MINUS);
+                return tok;
+
+            case 37:
+                // retorna token * (MULTI)
+                tok = new Token(MULTI);
+                return tok;
+
+            case 38:
+                // analisa se é um lexema = (ATTR) ou um == (EQ)
+                if(input[pos] == '=')
+                    state = 39;
+                else
+                    state = 300;
+                pos++;
+                break;
+
+            case 39:
+                // retorna token == (EQ)
+                tok = new Token(EQ);
+                return tok;
+
+            case 300:
+                // retorna token = (ATTR)
+                tok = new Token(ATTR);
+                pos--;
+                return tok;
+            
+            case 301:
+                // analisa se é um lexema ! (NOT) ou um != (NE)
+                if(input[pos] == '=')
+                    state = 302;
+                else
+                    state = 303;
+                pos++;
+                break;
+
+            case 302:
+                // retorna token != (NE)
+                tok = new Token(NE);
+                return tok;
+
+            case 303:
+                // retorna token ! (NOT)
+                tok = new Token(NOT);
+                pos--;
                 return tok;
 
             case 101:
-                //se for um comentario
+                //se for um comentario ou um operador
                 if (input[pos] == '/')
                     state = 102;
                 else if(input[pos] == '*')
                     state = 103;
+                else
+                    state = 30;
                 pos++;
                 break;
 
@@ -166,24 +287,27 @@ Scanner::lexicalError(string)
 
 string
 Scanner::returnName(int i){
-    string tokenNames[18] = {"UNDEF",
-                               "ID",
-                               "IF",
-                               "ELSE",
-                               "THEN",
-                               "RELOP",
-                               "EQ",
-                               "NE",
-                               "GT",
-                               "GE",
-                               "LT",
-                               "LE",
-                               "SEP",
-                               "NUMBER",
-                               "DOUBLE_LITERAL",
-                               "FLOAT_LITERAL",
-                               "INTEGER_LITERAL",
-                               "END_OF_FILE"};
+    string tokenNames[22] = {"UNDEF",
+                            "ID",
+                            "IF",
+                            "ELSE",
+                            "THEN",
+                            "RELOP",
+                            "EQ",
+                            "NE",
+                            "GT",
+                            "LT",
+                            "SEP",
+                            "NUMBER",
+                            "INTEGER_LITERAL",
+                            "AND",
+                            "PLUS",
+                            "MINUS",
+                            "MULTI",
+                            "DIV",
+                            "ATTR",
+                            "NOT",
+                            "END_OF_FILE"};
     return tokenNames[i];
     
 }
