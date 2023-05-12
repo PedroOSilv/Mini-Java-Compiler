df740ab6f2d8ca5757e656fc88a656c20a1c0854
pdrzanFilho <contatesteparalogins@gmail.com>
2023-05-12 14:30:43 -0400

    Parte 2 - Quase finalizada, falta apenas o expression

--- principal.cpp
+++ principal.cpp
@@ -1,31 +1,19 @@
-#include "scanner.cpp"
+#include "parser.h"
 
 int main(int argc, char* argv[]) 
 {
     //Verifica se foi executado corretamente
     //Essa main espera receber o nome do arquivo a ser
     //executado na linha de comando.
-    if (argc != 2){
-        
-        cout << "Uso: ./principal nome_arquivo.mj\n";
+    if (argc != 2)
+    {
+        cout<< "Uso: ./compiler nome_arquivo.mj\n";./
         return 1;
     }
 
-    string input;
+    Parser* parser = new Parser(argv[1]);
 
-    Scanner* scanner = new Scanner(argv[1]);
-    
-    Token* t;
-    
-    do{
-        t = scanner->nextToken();
-        
-        cout << scanner->returnName(t->name) + " ";
-        
-    }while (t->name != END_OF_FILE);
+    parser->run();
 
-    cout<< "\n";
-    delete scanner;
-    
-    return 0; 
+    return 0;
 }
